6df7730ac430cb52dc146992770d3a8baa075e1d
Dwi Permana Aji <dwipermanaaji@gmail.com>
2017-12-23 02:33:11 -0800

    Penambahan View
    -

--- application/controllers/Restoran/Pegawai/index.php
+++ application/controllers/Restoran/Pegawai/index.php
@@ -10,5 +10,11 @@ class index extends Ci_controller
 	{
 		parent::__construct();
 	}
+	function index(){
+		$this->load->view('restoran/pegawai/template/header');
+		$this->load->view('restoran/pegawai/template/menu');
+		$this->load->view('restoran/pegawai/isi');
+		$this->load->view('restoran/pegawai/template/footer');		
+	}
 }
  ?>
