4545d8f93e14ca1966eebcb4b4d41efa1392f3e8
YadiPrassetya <idayz77prs@gmail.com>
2018-01-26 16:11:17 +0700

    bentrok

--- assets/js/kitchen/kitchen.js
+++ assets/js/kitchen/kitchen.js
@@ -17,6 +17,28 @@ $(document).on("click", '.tombolalat', function(e){
 
 });
 
+$(document).on("click", '.tombolterima', function(e){
+	var kode_mkn			= $(this).data('kode_mkn');
+	var nama_mkn 		= $(this).data('nama_mkn');
+	var jumlah_masuk 	= $(this).data('jumlah_masuk');
+	var id_penerimaan 	= $(this).data('id_penerimaan');
+	var tanggal_masuk	= $(this).data('tanggal_masuk');
+
+
+	$(".kode_mkn").val(kode_mkn);
+	$(".nama_mkn").val(nama_mkn);
+	$(".id_penerimaan").val(id_penerimaan);
+	$(".tanggal_masuk").val(tanggal_masuk);
+	$(".jumlah_masuk").val(jumlah_masuk);
+	
+	
+	
+
+	$(".edit").text('Edit Data "' + nama_am +'"');
+	$(".delete").html('Apakah anda ingin menghapus data <strong>"'+nama_am+'"</strong> ?');
+
+});
+
 $(document).on("click", '.status1', function(e){
 	var id_order			= $(this).data('id_order');
 	var status_order 		= $(this).data('status_order');
@@ -26,7 +48,7 @@ $(document).on("click", '.status1', function(e){
 
 });
 
-$('.alert-success').show('fast').delay(5000).hide('fast');
+$('.alertalat').show('fast').delay(5000).hide('fast');
 
 
 $(document).on("change", '#cboItem', function(e){
