e5ab32acb2c4df3b0c5c5c6c809cb6188360dad8
guntaram22 <34910075+guntaram22@users.noreply.github.com>
2018-01-29 13:40:14 +0700

    update

--- application/views/restoran/pengelolaan_restoran/rinc_event.php
+++ application/views/restoran/pengelolaan_restoran/rinc_event.php
@@ -17,21 +17,6 @@
                 <div class="x_panel">
                   <div class="x_title">
                     <h2>Rincian Event</h2>
-                    <ul class="nav navbar-right panel_toolbox">
-                      <li><a class="collapse-link"><i class="fa fa-chevron-up"></i></a>
-                      </li>
-                      <li class="dropdown">
-                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false"><i class="fa fa-wrench"></i></a>
-                        <ul class="dropdown-menu" role="menu">
-                          <li><a href="#">Settings 1</a>
-                          </li>
-                          <li><a href="#">Settings 2</a>
-                          </li>
-                        </ul>
-                      </li>
-                      <li><a class="close-link"><i class="fa fa-close"></i></a>
-                      </li>
-                    </ul>
                     <div class="clearfix"></div>
                   </div>
                   <div class="x_content">
@@ -73,38 +58,32 @@
                                 <th>No</th>
                                 <th>Kebutuhan</th>
                                 <th>Jumlah</th>
-                                <th style="width: 59%">Description</th>
                               </tr>
                             </thead>
                             <tbody>
                               <tr>
                                 <td>1</td>
-                                <td>Call of Duty</td>
-                                <td>455-981-221</td>
-                                <td>El snort testosterone trophy driving gloves handsome gerry Richardson helvetica tousled street art master testosterone trophy driving gloves handsome gerry Richardson
-                                </td>
-                          
+                                <td>Meja Dinner</td>
+                                <td>20 set</td>
+                                
                               </tr>
                               <tr>
-                                <td>1</td>
-                                <td>Need for Speed IV</td>
-                                <td>247-925-726</td>
-                                <td>Wes Anderson umami biodiesel</td>
-                              
+                                <td>2</td>
+                                <td>Waiter</td>
+                                <td>5 orang</td>
+                                
                               </tr>
                               <tr>
-                                <td>1</td>
-                                <td>Monsters DVD</td>
-                                <td>735-845-642</td>
-                                <td>Terry Richardson helvetica tousled street art master, El snort testosterone trophy driving gloves handsome letterpress erry Richardson helvetica tousled</td>
-                         
+                                <td>3</td>
+                                <td>Minuman</td>
+                                <td>80</td>
+                                
                               </tr>
                               <tr>
-                                <td>1</td>
-                                <td>Grown Ups Blue Ray</td>
-                                <td>422-568-642</td>
-                                <td>Tousled lomo letterpress erry Richardson helvetica tousled street art master helvetica tousled street art master, El snort testosterone</td>
-                   
+                                <td>4</td>
+                                <td>Makanan</td>
+                                <td>80</td>
+                                
                               </tr>
                             </tbody>
                           </table>
