b60e51865c64f4fddef742fe5b9d78cc8ad0402d
dwipermanaaji <34787192+dwipermanaaji@users.noreply.github.com>
2018-02-01 07:15:30 +0700

    teu baleg
    ...

--- application/controllers/Restoran/Pegawai/Pembagian_tugas.php
+++ application/controllers/Restoran/Pegawai/Pembagian_tugas.php
@@ -16,11 +16,7 @@ class Pembagian_tugas extends ci_controller
 
 		$data['data_tugas'] = $this->Crud_model->select('kelolatugas');	
 		$data['data_pegawai'] = $this->Crud_model->select('pegawai');	
-<<<<<<< HEAD
-		$data['pembagian_tugas'] = $this->Crud_model->inner_join3('kontol_kerja','pegawai','kelolatugas','id_pegawai','id_tugas');
-=======
 		$data['pembagian_tugas'] = $this->Crud_model->inner_join3('kontrol_kerja','pegawai','kelolatugas','id_pegawai','id_tugas');
->>>>>>> fd58cdfb379aadf2ff2c70a0f8bf8325c698e7e0
 		$this->load->view('restoran/pegawai/template/header');
 		$this->load->view('restoran/pegawai/template/menu');
 		$this->load->view('restoran/pegawai/Pembagian_tugas_view',$data);
@@ -33,11 +29,7 @@ class Pembagian_tugas extends ci_controller
 			'status_ck' => $this->input->post('status_ck')
 			);
 
-<<<<<<< HEAD
-		$this->Crud_model->insert('kontol_kerja',$data);
-=======
 		$this->Crud_model->insert('kontrol_kerja',$data);
->>>>>>> fd58cdfb379aadf2ff2c70a0f8bf8325c698e7e0
 		echo "<script type='text/javascript'>alert('Data ID Pegawai = ".$this->input->post('id_pegawai')." Berhasil di Tambahkan');</script>";	
 		$link = base_url().'Restoran/pegawai/Pembagian_Tugas';		
 		redirect($link,'refresh');
@@ -49,11 +41,7 @@ class Pembagian_tugas extends ci_controller
 			'id_tugas' => $this->input->post('tugas')
 			);
 		$where = array('no' => $this->input->post('no'));
-<<<<<<< HEAD
-		$this->Crud_model->update('kontol_kerja', $data, $where);
-=======
 		$this->Crud_model->update('kontrol_kerja', $data, $where);
->>>>>>> fd58cdfb379aadf2ff2c70a0f8bf8325c698e7e0
 		echo "<script type='text/javascript'>alert('Data Berhasil di Sunting');</script>";		
 		$link = base_url().'Restoran/pegawai/Pembagian_Tugas';
 		redirect($link,'refresh');
@@ -61,11 +49,7 @@ class Pembagian_tugas extends ci_controller
 	}
 	function hapus_pegawai($id){
 		$where = array('no' => $id);
-<<<<<<< HEAD
-		$this->Crud_model->delete('kontol_kerja', $where);
-=======
 		$this->Crud_model->delete('kontrol_kerja', $where);
->>>>>>> fd58cdfb379aadf2ff2c70a0f8bf8325c698e7e0
 		echo "<script type='text/javascript'>alert('Data Berhasil di Hapus');</script>";		
 		$link = base_url().'Restoran/pegawai/Pembagian_Tugas';
 		redirect($link,'refresh');		
